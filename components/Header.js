0cd01241131d6254cf6ac053be0dfcbbb0276f33
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-11-20 10:44:04 +0530

    added header component

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,47 @@
+import Image from "next/image";
+import {
+    BellIcon,
+    ChatIcon,
+    ChevronDownIcon,
+    HomeIcon,
+    UserGroupIcon,
+    ViewGridIcon,
+} from "@heroicons/react/solid";
+
+import {
+    FlagIcon,
+    PlayIcon,
+    SearchIcon,
+    shoppingCartIcon,
+} from "@heroicons/react/outline";
+import HeaderIcon from "./HeaderIcon";
+
 function Header() {
     return (
         <div>
-            
+            {/* Left */}
+            <div className="flex items-center">
+                <Image 
+                    src="https://links.papareact.com/5me" 
+                    width={40}
+                    height={40}
+                    layout="fixed"
+                    alt="facebook-logo"
+                />
+
+                <div className="flex ml-2 items-center rounded-full bg-gray-100">
+                    <SearchIcon className="h-6 text-gray-600" />
+                    <input className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500" type="text" placeholder="Search Facebook" />
+                </div>
+            </div>
+            {/* Center */}
+            <div className="flex justify-center flex-grow">
+                <div>
+                    <HeaderIcon Icon={HomeIcon} />
+                </div>
+            </div>
+
+            {/* Right */}
         </div>
     )
 }
