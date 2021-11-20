3b6dafc1ef9b167311eb66df582cd8e82aac47a5
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-11-20 10:49:28 +0530

    added header feature

--- components/Header.js
+++ components/Header.js
@@ -4,6 +4,7 @@ import {
     ChatIcon,
     ChevronDownIcon,
     HomeIcon,
+    ShoppingCartIcon,
     UserGroupIcon,
     ViewGridIcon,
 } from "@heroicons/react/solid";
@@ -18,32 +19,40 @@ import HeaderIcon from "./HeaderIcon";
 
 function Header() {
     return (
-        <div>
-            {/* Left */}
-            <div className="flex items-center">
-                <Image 
-                    src="https://links.papareact.com/5me" 
-                    width={40}
-                    height={40}
-                    layout="fixed"
-                    alt="facebook-logo"
-                />
+      <div>
+        {/* Left */}
+        <div className="flex items-center">
+          <Image
+            src="https://links.papareact.com/5me"
+            width={40}
+            height={40}
+            layout="fixed"
+            alt="facebook-logo"
+          />
 
-                <div className="flex ml-2 items-center rounded-full bg-gray-100">
-                    <SearchIcon className="h-6 text-gray-600" />
-                    <input className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500" type="text" placeholder="Search Facebook" />
-                </div>
-            </div>
-            {/* Center */}
-            <div className="flex justify-center flex-grow">
-                <div>
-                    <HeaderIcon Icon={HomeIcon} />
-                </div>
-            </div>
-
-            {/* Right */}
+          <div className="flex ml-2 items-center rounded-full bg-gray-100">
+            <SearchIcon className="h-6 text-gray-600" />
+            <input
+              className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500"
+              type="text"
+              placeholder="Search Facebook"
+            />
+          </div>
         </div>
-    )
+        {/* Center */}
+        <div className="flex justify-center flex-grow">
+          <div>
+            <HeaderIcon Icon={HomeIcon} />
+            <HeaderIcon Icon={FlagIcon} />
+            <HeaderIcon Icon={PlayIcon} />
+            <HeaderIcon Icon={ShoppingCartIcon} />
+            <HeaderIcon Icon={UserGroupIcon} />
+          </div>
+        </div>
+
+        {/* Right */}
+      </div>
+    );
 }
 
 export default Header;
