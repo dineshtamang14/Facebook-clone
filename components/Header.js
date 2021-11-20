3006bb33404c224de57b52a4e81bb0363c33eb42
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-11-20 15:44:27 +0530

    added login component

--- components/Header.js
+++ components/Header.js
@@ -19,7 +19,7 @@ import HeaderIcon from "./HeaderIcon";
 
 function Header() {
     return (
-      <div>
+      <div className="sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md">
         {/* Left */}
         <div className="flex items-center">
           <Image
@@ -30,10 +30,10 @@ function Header() {
             alt="facebook-logo"
           />
 
-          <div className="flex ml-2 items-center rounded-full bg-gray-100">
+          <div className="flex ml-2 pl-2 items-center rounded-full bg-gray-100">
             <SearchIcon className="h-6 text-gray-600" />
             <input
-              className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500"
+              className="hidden md:inline-flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink"
               type="text"
               placeholder="Search Facebook"
             />
@@ -41,8 +41,8 @@ function Header() {
         </div>
         {/* Center */}
         <div className="flex justify-center flex-grow">
-          <div>
-            <HeaderIcon Icon={HomeIcon} />
+          <div className="flex space-x-6 md:space-x-2">
+            <HeaderIcon active Icon={HomeIcon} />
             <HeaderIcon Icon={FlagIcon} />
             <HeaderIcon Icon={PlayIcon} />
             <HeaderIcon Icon={ShoppingCartIcon} />
@@ -51,6 +51,15 @@ function Header() {
         </div>
 
         {/* Right */}
+        <div className="flex items-center sm:space-x-2 justify-end">
+          {/* Profile photo */}
+          <ViewGridIcon className="icon" />
+          <ChatIcon className="icon" />
+          <BellIcon className="icon" />
+          <ChevronDownIcon className="icon" />
+          <p className="whitespace-nowrap font-semibold pr-3">Dinesh Tamang</p>
+        </div>
+
       </div>
     );
 }
