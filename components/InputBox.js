357012a8d1f3465334ca3f764e17af059f42e7de
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-11-21 15:26:33 +0530

    added feed  component

---
+++ components/InputBox.js
@@ -0,0 +1,49 @@
+import { useSession } from "next-auth/client";
+import Image from "next/image";
+import {EmojiHappyIcon} from "@heroicons/react/outline";
+import {cameraIcon, VideoCameraIcon} from "@heroicons/react/solid";
+
+function InputBox() {
+    const [session] = useSession();
+
+    const sendPost = (e) => {
+        e.preventDefault();
+    }
+
+    return (
+        <div className="bg-white p-2 rounded-2xl shadow-md text-gray-500 font-medium mt-6">
+            <div className="flex space-x-4 p-4 items-center">
+                <Image 
+                    className="rounded-full"
+                    src={session.user.image}
+                    width={40}
+                    height={40}
+                    layout="fixed"
+                    alt="profile-pic"
+                />
+
+                <form className="flex flex-1">
+                    <input type="text" 
+                        className="rounded-full h-12 bg-gray-100 flex-grow px-5 focus:outline-none"
+                        placeholder={`What's on your mind, ${session.user.name}?`}
+                    />
+                    <button hidden type="submit" onClick={sendPost}>Submit</button>
+                </form>
+            </div>
+
+            <div>
+                <div>
+
+                </div>
+                <div>
+
+                </div>
+                <div>
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default InputBox;
